14a28f8a62c60ed4c0c6ba601a83a90b5acf70ab
David Paskevic <davids.paskevics@gmail.com>
2020-01-15 19:27:51 +0100

    Add udev rule for RISC-V dev board

--- roles/subroles/dev.nix
+++ roles/subroles/dev.nix
@@ -51,5 +51,6 @@
   services.lorri.enable = true;
   
   programs.adb.enable = true;
+services.udev.extraRules = "ATTRS{idVendor}''\=''\"28e9''\", ATTRS{idProduct}''\=''\"0189''\", MODE=''\"0666''\", ENV{ID_MM_DEVICE_IGNORE}''\=''\"1''\", ENV{IF_MM_PORT_IGNORE}''\=''\"1''\"";
  
 }
