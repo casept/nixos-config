0c93e2db5b5a1132ac98324226e0765b2e931e6f
casept <davids.paskevics@gmail.com>
2020-07-20 15:04:48 +0200

    Update niv pins

--- shell.nix
+++ shell.nix
@@ -2,10 +2,13 @@ let
   # Use a pinned nixpkgs version with niv
   sources = import ./nix/sources.nix;
   nixpkgs = import sources.nixpkgs { };
+  niv = import sources.niv { };
   ng = import sources.nixos-generators { };
   shellPackages = [
     nixpkgs.nixops
     sources.nixos-shell
     ng
+    niv.niv
+    nixpkgs.nixops
   ];
 in nixpkgs.mkShell { buildInputs = shellPackages; }
