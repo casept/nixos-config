c6b98cf9db41b16c7f8a3673bfd21c5ca4fb0991
casept <davids.paskevics@gmail.com>
2024-10-22 01:16:46 +0200

    Remove no longer used machines

--- flake.nix
+++ flake.nix
@@ -53,35 +53,6 @@
         { # pkgs.comma = (super.callPackage comma { });
         };
     in {
-      nixosConfigurations.casept-x230t = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ({ lib, config, pkgs, ... }: {
-            imports = [
-              # These should be imported in the hardware/role modules, but that causes infinite recursion
-              home-manager.nixosModules.home-manager
-              nixos-hardware.nixosModules.lenovo-thinkpad-x230
-
-              (import ./boxes/casept-x230t.nix {
-                inherit pkgs lib comma nixpkgs nixpkgs-unstable nixos-hardware
-                  home-manager nixos-vsliveshare rnix-lsp-flake;
-              })
-              (import ./common/flake-conf.nix {
-                inherit pkgs nixpkgs nixpkgs-unstable;
-              })
-            ];
-            nixpkgs.overlays = [ overlay-unstable overlay-mullvad extra-pkgs ];
-            nixpkgs.config.allowUnfree = true;
-            nixpkgs.config.allowBroken = true;
-            nixpkgs.config.pipewire = true; # Needed for waybar PA widget
-            # Let 'nixos-version --json' know about the Git revision of this flake.
-            system.configurationRevision =
-              nixpkgs.lib.mkIf (self ? rev) self.rev;
-            nix.registry.nixpkgs.flake = nixpkgs;
-          })
-        ];
-      };
-
       nixosConfigurations.l13 = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
@@ -111,34 +82,6 @@
         ];
       };
 
-      nixosConfigurations.t400-homeserver = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ({ lib, config, pkgs, ... }: {
-            imports = [
-              # These should be imported in the hardware/role modules, but that causes infinite recursion
-              home-manager.nixosModules.home-manager
-              nixos-hardware.nixosModules.common-cpu-intel
-              nixos-hardware.nixosModules.common-pc-laptop-hdd
-              nixos-hardware.nixosModules.common-pc-laptop
-
-              (import ./boxes/t400-homeserver.nix {
-                inherit config pkgs lib nixpkgs nixpkgs-unstable nixos-hardware
-                  home-manager comma nixos-vsliveshare rnix-lsp-flake;
-              })
-              (import ./common/flake-conf.nix {
-                inherit pkgs nixpkgs nixpkgs-unstable;
-              })
-            ];
-            nixpkgs.overlays = [ overlay-unstable ];
-            nixpkgs.config.allowUnfree = true;
-            # Let 'nixos-version --json' know about the Git revision of this flake.
-            system.configurationRevision =
-              nixpkgs.lib.mkIf (self ? rev) self.rev;
-            nix.registry.nixpkgs.flake = nixpkgs;
-          })
-        ];
-      };
       # TODO: Load dev shell with tools for working on this config
     };
 }
