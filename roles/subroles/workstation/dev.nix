cdf8762133e3bedfef09a2119de2bc8029bd28c3
casept <davids.paskevics@gmail.com>
2024-10-24 21:07:34 +0200

    Add some programming tools

--- roles/subroles/workstation/dev.nix
+++ roles/subroles/workstation/dev.nix
@@ -1,7 +1,20 @@
-{ config, pkgs, ... }: {
+{ config, environment, pkgs, ... }: {
   programs.adb.enable = true;
+
   # Udev rules for programming a RISC-V dev board from china
   services.udev.extraRules =
     "ATTRS{idVendor}''=''\"28e9''\", ATTRS{idProduct}''=''\"0189''\", MODE=''\"0666''\", ENV{ID_MM_DEVICE_IGNORE}''=''\"1''\", ENV{IF_MM_PORT_IGNORE}''=''\"1''\"";
 
+
+  environment.systemPackages = with pkgs; [
+    # Common programming tools I always want available
+    unstable.gcc
+    unstable.clang
+    unstable.clang-tools
+    unstable.ccacheWrapper
+
+    unstable.cargo
+
+    python3
+  ];
 }
