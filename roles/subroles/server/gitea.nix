50e2ce077936555368b8ad5b131bf8e473fdde7b
casept <davids.paskevics@gmail.com>
2025-01-18 14:14:58 +0100

    Remove leftovers of old server config

--- roles/subroles/server/gitea.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }: {
-  system.stateVersion = "23.05";
-  services.gitea = {
-    enable = true;
-    stateDir = "/persist";
-  };
-  networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 22 80 ];
-}
