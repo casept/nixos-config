389a76e53b7f5460ccf0aaf0f9f37ef4f698a744
casept <davids.paskevics@gmail.com>
2025-01-21 00:14:35 +0100

    clobus: Add remote spline builder

--- boxes/clobus.nix
+++ boxes/clobus.nix
@@ -95,4 +95,23 @@
     group = "nogroup";
   };
   services.displayManager.hiddenUsers = [ "nixremote" ];
+
+  nix.buildMachines = [{
+    # Spline remote builder
+    # remote-program cannot be passed as an option by itself, but can be put here as
+    # it's just concatenated together internally:
+    # https://github.com/NixOS/nixpkgs/blob/6a3ae7a5a12fb8cac2d59d7df7cbd95f9b2f0566/nixos/modules/config/nix-remote-build.nix#L32
+    hostName = "vm-build-big?remote-program=/nix/var/nix/profiles/default/bin/nix-daemon";
+    sshUser = "nixremote";
+    systems = [ "x86_64-linux" "i686-linux" ];
+    maxJobs = 40;
+    speedFactor = 20;
+    supportedFeatures = [ "benchmark" "big-parallel" ];
+    mandatoryFeatures = [ ];
+    protocol = "ssh-ng";
+  }];
+  nix.distributedBuilds = true;
+  nix.extraOptions = ''
+    builders-use-substitutes = true
+  '';
 }
