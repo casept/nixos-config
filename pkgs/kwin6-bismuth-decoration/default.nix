44eab1e9d935853db110184a75d2d873c4ebe63b
casept <davids.paskevics@gmail.com>
2025-12-05 01:08:10 +0100

    kwin6-bismuth-window-decoration: Fix build

--- pkgs/kwin6-bismuth-decoration/default.nix
+++ pkgs/kwin6-bismuth-decoration/default.nix
@@ -4,16 +4,16 @@ stdenv.mkDerivation {
   version = "master";
 
   src = fetchFromGitHub {
-    owner = "ivan-cukic";
+    # Until https://github.com/ivan-cukic/kwin6-bismuth-decoration/pull/3 is merged
+    owner = "casept";
     repo = "kwin6-bismuth-decoration";
-    rev = "ab6755fa1ca8d61535ff41e6a60d82b680847008";
-    sha256 = "sha256-AXIyBRAW16pPVxWbhRcecCi9Lf3MsQMakSZD+a+BtSU=";
+    rev = "857731860de66b2f5803022c952d576310934ce0";
+    sha256 = "sha256-iANKNwzmD7BORsM9AQygjuSpWMfLfIxXRkr6JVkCvzI=";
   };
 
   nativeBuildInputs = [ cmake kdePackages.extra-cmake-modules pkg-config ];
   buildInputs = [ kdePackages.kcmutils kdePackages.kconfig kdePackages.kconfigwidgets kdePackages.kcoreaddons kdePackages.kdeclarative kdePackages.kdecoration kdePackages.kglobalaccel kdePackages.kcmutils kdePackages.ki18n kdePackages.qtbase kdePackages.qtdeclarative kdePackages.qtsvg ];
   dontWrapQtApps = true;
-
   meta = with lib; {
     description = "Bismuth window decoration";
     homepage = "https://github.com/ivan-cukic/kwin6-bismuth-decoration";
